f05c156162a4d990dff5a91c0ee21016ff004510
TheSBGames <thesdgams07@gmail.com>
2025-07-17 20:43:23 +0530

    Create parseDiration.js

---
+++ utils/parseDiration.js
@@ -0,0 +1,15 @@
+module.exports = function parseDuration(duration) {
+    const match = duration.match(/^(\d+)(s|m|h|d)$/);
+    if (!match) return null;
+
+    const value = parseInt(match[1]);
+    const unit = match[2];
+
+    switch (unit) {
+        case 's': return value * 1000;
+        case 'm': return value * 60 * 1000;
+        case 'h': return value * 60 * 60 * 1000;
+        case 'd': return value * 24 * 60 * 60 * 1000;
+        default: return null;
+    }
+};
