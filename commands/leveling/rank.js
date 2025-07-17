84dfaa7c61f852de7480e6fa3f824c46869f9c09
TheSBGames <thesdgams07@gmail.com>
2025-07-17 21:04:01 +0530

    Update rank.js

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,1 +1,36 @@
-// Rank command
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const levelSchema = require('../../models/level');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('rank')
+    .setDescription('Check your or someone else\'s rank.')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('The user to check')
+        .setRequired(false)),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser('user') || interaction.user;
+
+    const data = await levelSchema.findOne({
+      guildId: interaction.guild.id,
+      userId: user.id
+    });
+
+    if (!data) {
+      return interaction.reply({ content: `${user.username} has no XP yet.`, ephemeral: true });
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(`${user.username}'s Rank`)
+      .setColor('Blurple')
+      .addFields(
+        { name: 'Level', value: `${data.level}`, inline: true },
+        { name: 'XP', value: `${data.xp}`, inline: true }
+      )
+      .setThumbnail(user.displayAvatarURL());
+
+    interaction.reply({ embeds: [embed] });
+  }
+};
