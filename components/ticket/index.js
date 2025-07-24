3e000ea21b8261afd70f61a0ea7d31803035466f
TheSBGames <thesdgams07@gmail.com>
2025-07-24 19:35:43 +0530

    Create index.js

---
+++ components/ticket/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ticketMenu: require('./menu.js'),
+  ticketPanel: require('./panel.js'),
+  closeButton: require('./close-button.js'),
+  confirmCloseButtons: require('./confirm-close.js'),
+  transcriptButton: require('./transcript-button.js'),
+  setup: require('./setup.js'),
+};
