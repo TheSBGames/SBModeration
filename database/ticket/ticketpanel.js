0b3f39d6bc97852076ccef54a641c7a93351ea47
TheSBGames <thesdgams07@gmail.com>
2025-07-25 20:01:44 +0530

    Update ticketpanel.js

--- database/ticket/ticketpanel.js
+++ database/ticket/ticketpanel.js
@@ -5,7 +5,7 @@ const TicketPanelSchema = new mongoose.Schema({
   panelId: { type: String, required: true },
   title: { type: String },
   description: { type: String },
-  categoryId: { type: String }, // NEW: Category channel ID to create tickets under
+  categoryId: { type: String }, // Optional: where to create ticket channels
   customButtonLabel: { type: String },
   customButtonEmoji: { type: String }
 });
