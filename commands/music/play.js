192791ecece53736c29e2b4b85b860e2ec794c6d
TheSBGames <thesdgams07@gmail.com>
2025-07-25 19:31:34 +0530

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,50 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { QueryType } = require('discord-player');
+const playerButtons = require("../../components/music/playerButtons");
+const loopButtonRow = require("../../components/music/loopButtonRow");
+const generateQueueSelect = require("../../components/music/generateQueueSelect");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('play')
-    .setDescription('Play a song or playlist')
-    .addStringOption(option =>
-      option.setName('query').setDescription('The name or URL of the song').setRequired(true)
-    ),
-
-  async execute(interaction, client) {
-    const query = interaction.options.getString('query');
-    const voiceChannel = interaction.member.voice.channel;
-
-    if (!voiceChannel) {
-      return interaction.reply({ content: '❌ You must be in a voice channel to use this command.', ephemeral: true });
-    }
-
-    const queue = await client.player.nodes.create(interaction.guild, {
-      metadata: interaction.channel,
-      leaveOnEnd: true,
-      leaveOnEmptyCooldown: 300000,
-      leaveOnStop: true,
-    });
-
-    try {
-      await queue.connect(voiceChannel);
-    } catch {
-      client.player.nodes.delete(interaction.guildId);
-      return interaction.reply({ content: '❌ Failed to join your voice channel!', ephemeral: true });
-    }
-
-    const result = await client.player.search(query, {
-      requestedBy: interaction.user,
-      searchEngine: QueryType.AUTO,
-    });
-
-    if (!result.tracks.length) return interaction.reply({ content: '❌ No results found!', ephemeral: true });
-
-    const track = result.playlist ? result.tracks : result.tracks[0];
-
-    if (result.playlist) queue.addTrack(result.tracks);
-    else queue.addTrack(track);
-
-    if (!queue.isPlaying()) await queue.node.play();
-
-    return interaction.reply(`🎶 Added **${result.playlist ? result.playlist.title : track.title}** to the queue!`);
-  },
-};
+await interaction.reply({
+  content: `🎵 Now playing: **${track.title}**`,
+  components: [
+    playerButtons(),
+    loopButtonRow(),
+    generateQueueSelect(queue)
+  ]
+});
