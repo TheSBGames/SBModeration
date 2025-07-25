db36076bea0a94065791d90143b745096bb07c43
TheSBGames <thesdgams07@gmail.com>
2025-07-25 19:29:52 +0530

    Update playerbuttons.js

--- components/music/playerbuttons.js
+++ components/music/playerbuttons.js
@@ -2,10 +2,10 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = () => {
   return new ActionRowBuilder().addComponents(
-    new ButtonBuilder().setCustomId("pause").setLabel("Pause").setStyle(ButtonStyle.Secondary).setEmoji("⏸️"),
-    new ButtonBuilder().setCustomId("resume").setLabel("Resume").setStyle(ButtonStyle.Success).setEmoji("▶️"),
-    new ButtonBuilder().setCustomId("skip").setLabel("Skip").setStyle(ButtonStyle.Primary).setEmoji("⏭️"),
-    new ButtonBuilder().setCustomId("shuffle").setLabel("Shuffle").setStyle(ButtonStyle.Secondary).setEmoji("🔀"),
-    new ButtonBuilder().setCustomId("stop").setLabel("Stop").setStyle(ButtonStyle.Danger).setEmoji("⏹️")
+    new ButtonBuilder().setCustomId("pause").setEmoji("⏸️").setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder().setCustomId("resume").setEmoji("▶️").setStyle(ButtonStyle.Success),
+    new ButtonBuilder().setCustomId("skip").setEmoji("⏭️").setStyle(ButtonStyle.Primary),
+    new ButtonBuilder().setCustomId("shuffle").setEmoji("🔀").setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder().setCustomId("stop").setEmoji("⏹️").setStyle(ButtonStyle.Danger)
   );
 };
